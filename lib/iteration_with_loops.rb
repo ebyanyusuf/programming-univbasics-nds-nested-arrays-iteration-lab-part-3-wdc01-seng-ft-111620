9e66126ca5ae0aa758a04d09ce428d064ce8ff7a
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-09 19:59:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].is_a?(String)
-      src[row_index][element_index] >> string_array
+      string_array 
       
     end
      
